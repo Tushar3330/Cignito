9eae4eb3f9092d2ac611479e2ad3aaac8b5ebf8a
Tushar3330 <tusharrana2505@gmail.com>
2025-09-04 21:01:01 +0530

    a simple navbar prototype has been created using next auth and github

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,13 +1,13 @@
-import NextAuth from "next-auth";
-import Github from "next-auth/providers/github";
-import GitHubProvider from "next-auth/providers/github";
+// app/lib/auth.ts
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
 
-export const {handlers , signIn , signOut , auth} = NextAuth({
-    providers : [
-        Github({
-            clientId: process.env.AUTH_GITHUB_ID || "",
-            clientSecret: process.env.AUTH_GITHUB_SECRET || ""  
-        })
-    ],
-    // Add any additional NextAuth configuration options here
-});
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+  ],
+})
+  
