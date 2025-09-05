3be4c6220f7ef294f64cfa5316c7061a528e8349
Tushar3330 <tusharrana2505@gmail.com>
2025-09-05 14:17:26 +0530

    included typesafety for startup type card it generates type directtly using typegen feature of sanity

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,13 +1,23 @@
-
-import React from 'react'
+import { cn, formatDate } from "@/lib/utils";
 import { EyeIcon } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
-import { formatDate } from "../lib/utils";
-import { Button } from "./ui/button";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Author, Startup } from "@/sanity/types";
 
-const StartupCard = ({post}:{post:StartupTypeCard}) => {
-  const {_createdAt, views, author :{_id:authorId, name , image:authorImage}, _id, title, description, image, category} = post;
+export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
+
+const StartupCard = ({ post }: { post: StartupTypeCard }) => {
+  const {
+    _createdAt,
+    views,
+    author,
+    title,
+    category,
+    _id,
+    image,
+    description,
+  } = post;
 
   return (
     <li className="startup-card group">
@@ -21,17 +31,17 @@ const StartupCard = ({post}:{post:StartupTypeCard}) => {
 
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${authorId}`}>
-            <p className="text-16-medium line-clamp-1">{name}</p>
+          <Link href={`/user/${author?._id}`}>
+            <p className="text-16-medium line-clamp-1">{author?.name}</p>
           </Link>
           <Link href={`/startup/${_id}`}>
             <h3 className="text-26-semibold line-clamp-1">{title}</h3>
           </Link>
         </div>
-        <Link href={`/user/${authorId}`}>
+        <Link href={`/user/${author?._id}`}>
           <Image
-            src={authorImage || "/default-profile.png"}
-            alt={name}
+            src={author?.image!}
+            alt={author?.name!}
             width={48}
             height={48}
             className="rounded-full"
@@ -56,5 +66,7 @@ const StartupCard = ({post}:{post:StartupTypeCard}) => {
     </li>
   );
 };
-  
-export default StartupCard
+
+
+
+export default StartupCard;
